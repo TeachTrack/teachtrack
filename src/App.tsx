abb293109235b88b85ac1ea586b4fdfadbcdaae1
Hojiakbardevs <abdulhakimovhojiakbar123@gmail.com>
2024-07-17 12:07:40 +0500

    TEA-23: Implement Not-found, Access Dedied, Server Error Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/login/login";
 import DashboardLayout from "./layout/dashboard-layout/dashboard-layout";
-import NotPage from "./pages/login/NotPage";
-import NotAcess from "./pages/login/NotAcess";
 import ServerEror from "./pages/login/ServerEror";
+import AccessDenied from "./pages/login/AccessDenied";
+import NotFound from "./pages/login/NotFound";
 
 const App = () => {
   return (
@@ -11,8 +11,8 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/Login" element={<LoginPage />} />
-          <Route path="*" element={<NotPage />} />
-          <Route path="/403" element={<NotAcess />}/>
+          <Route path="/403" element={<AccessDenied />} />
+          <Route path="*" element={<NotFound />}/>
           <Route path="/500" element={<ServerEror />}/>
           <Route
             path="/dashboard/*" element={
--- src/pages/login/NotFound.tsx
+++ src/pages/login/NotFound.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 import NotPageimg from "./../../assets/404.svg"
 
-const NotPage = () => {
+const NotFound = () => {
     return (
         <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
             <div className="text-center">
@@ -26,4 +26,4 @@ const NotPage = () => {
     )
 }
 
-export default NotPage
+export default NotFound
