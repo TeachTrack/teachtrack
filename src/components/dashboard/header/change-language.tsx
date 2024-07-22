697a6bfdab904cb10d4332a68f304b305c9dc6b4
Hojiakbardevs <abdulhakimovhojiakbar123@gmail.com>
2024-07-22 05:35:37 +0500

    TEA-29: change-language fix problem

--- src/components/dashboard/header/change-language.tsx
+++ src/components/dashboard/header/change-language.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import type { MenuProps } from "antd";
 import { Dropdown, Space } from "antd";
 import { ZhihuOutlined } from "@ant-design/icons";
@@ -6,46 +6,41 @@ import UzbFlag from "./../../../../public/uzb.svg";
 import RusFlag from "./../../../../public/rus.svg";
 import { useLocaleContext } from "../../../context/locale.context";
 
-// Define changeLanguage outside the component and use useCallback for optimization
-const changeLanguage = (language: string) => {
-  localStorage.setItem("language", language); // Save the language to localStorage
-  window.location.reload();
-};
-
-const items: MenuProps["items"] = [
-  {
-    label: (
-      <Space>
-        <img src={UzbFlag} alt="Uzb" width={30} height={20} />
-        O'zbek
-      </Space>
-    ),
-    key: "0",
-    onClick: () => changeLanguage("uz"),
-  },
-  {
-    label: (
-      <Space>
-        <img src={RusFlag} alt="Rus" width={30} height={20} />
-        Русский
-      </Space>
-    ),
-    key: "1",
-    onClick: () => changeLanguage("ru"),
-  },
-  {
-    type: "divider",
-  },
-];
-
 export const ChangeLanguage: React.FC = () => {
-  const [language, setLanguage] = React.useState(localStorage.getItem("language") || "uz");
+  const { t, setLang } = useLocaleContext();
 
-  React.useEffect(() => {
-    setLanguage(localStorage.getItem(language) || "uz");
-  }, []);
+  const changeLanguage = useCallback(
+    (language: string) => {
+      setLang(language);
+    },
+    [setLang],
+  );
 
-  const { t } = useLocaleContext();
+  const items: MenuProps["items"] = [
+    {
+      label: (
+        <Space>
+          <img src={UzbFlag} alt="Uzb" width={30} height={20} />
+          O'zbek
+        </Space>
+      ),
+      key: "0",
+      onClick: () => changeLanguage("uz"),
+    },
+    {
+      label: (
+        <Space>
+          <img src={RusFlag} alt="Rus" width={30} height={20} />
+          Русский
+        </Space>
+      ),
+      key: "1",
+      onClick: () => changeLanguage("ru"),
+    },
+    {
+      type: "divider",
+    },
+  ];
 
   return (
     <Dropdown menu={{ items }} trigger={["click"]}>
